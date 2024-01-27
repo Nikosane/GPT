4e8a5ab8b6ffc093c78e130dbbc553842c1f2b50
Nikosane <linux.nikosane@gmail.com>
2024-01-27 22:50:21 +0530

    setting the hyper parameter

--- Data/gpt.py
+++ Data/gpt.py
@@ -3,3 +3,14 @@ import torch
 from torch.nn import functional as F
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+eval_iters = 200
+batch_size = 32
+block_size = 64
+max_iters = 15000
+eval_interval = 1000
+learning_rate = 1e-3
+n_embd = 256
+n_head = 4
+n_layer = 8
+dropout = 0.2
