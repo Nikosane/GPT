2e5e87e8bb49a9b5f6f33e4252a11944ccb25945
Nikosane <linux.nikosane@gmail.com>
2024-01-27 23:03:34 +0530

    char encode and decode

--- Data/gpt.py
+++ Data/gpt.py
@@ -20,4 +20,9 @@ with open('/content/university data','r', encoding='utf-8') as f:
 text = ' '.join(text.split())
 
 chars = sorted(list(set(text)))
-vocab_size = len(chars) 
+vocab_size = len(chars)
+
+stoi = { ch:i for i,ch in enumerate(chars)}
+itos = { i:ch for i,ch in enumerate(chars)}
+encode = lambda s: [stoi[c] for c in s]
+decode = lambda l: ''.join([itos[i] for i in l])
