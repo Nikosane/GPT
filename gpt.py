193e237c18d3b92595a8e4060536b3399f7a90d9
Nikosane <linux.nikosane@gmail.com>
2024-01-28 23:42:40 +0530

    estimate loss

--- gpt.py
+++ gpt.py
@@ -46,4 +46,11 @@ def extimate_loss():
     model.eval()
     for split in ['train' , 'val']:
         losses = torch.zeros(eval_iters)
-        
+        for k in range(eval_iters):
+            X , Y = get_batch(split)
+            logits , loss = model(X, Y)
+            losses[k] = loss.item()
+        out[split] = losses.mean()
+    model.train()
+    return out 
+
