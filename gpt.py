763c5723ce89cc80ac64ded48c1d5f024fb097f2
Nikosane <linux.nikosane@gmail.com>
2024-01-28 21:57:07 +0530

    batch function

--- gpt.py
+++ gpt.py
@@ -31,3 +31,11 @@ data = torch.tensor(encode(text), dtype=torch.long)
 n = int(0.9*len(data))
 train_data = data[:n]
 val_data = data[n:]
+
+def get_batch(split):
+    data = train_data if split == 'train' else val_data
+    ix = torch.randint(len(data)- block_size , (batch_size,))
+    x = torch.stack([data[i:i+block_size] for i in ix])
+    y = torch.stack([data[i+1:i+block_size+1] for i in ix])
+    x, y = x.to(device) , y.to(device)
+    return x, y
