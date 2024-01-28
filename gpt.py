0e6e447f1061ba2528a9943c54710c261fb5139a
Nikosane <linux.nikosane@gmail.com>
2024-01-28 21:38:20 +0530

    train and test split

--- gpt.py
+++ gpt.py
@@ -26,3 +26,8 @@ stoi = { ch:i for i,ch in enumerate(chars)}
 itos = { i:ch for i,ch in enumerate(chars)}
 encode = lambda s: [stoi[c] for c in s]
 decode = lambda l: ''.join([itos[i] for i in l])
+
+data = torch.tensor(encode(text), dtype=torch.long)
+n = int(0.9*len(data))
+train_data = data[:n]
+val_data = data[n:]
