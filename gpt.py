3b2e524fb2685dfd1c2d577cecca3ea3c0944a28
Nikosane <linux.nikosane@gmail.com>
2024-01-30 23:39:22 +0530

    forward

--- gpt.py
+++ gpt.py
@@ -121,5 +121,10 @@ class Block(nn.Module):
         self.ln1 = nn.LayerNorm(n_embd)
         self.ln2 = nn.LayerNorm(n_embd)
 
+    def forward(self, x):
+        x = x + self.sa(self.ln1(x))
+        x = x + self.ffwd(self.ln2(x))
+        return x
+
     
 
