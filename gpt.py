ae856293540d6ce51324da2722fa2a8920a9eafd
Nikosane <linux.nikosane@gmail.com>
2024-01-30 23:37:50 +0530

    block

--- gpt.py
+++ gpt.py
@@ -108,4 +108,18 @@ class FeedFoward(nn.Module):
 
     def forward(self, x):
         return self.net(x)
+    
+
+class Block(nn.Module):
+    """ Transformer block: communication followed by computation """
+
+    def __init__(self, n_embd, n_head):
+        super().__init__()
+        head_size = n_embd // n_head
+        self.sa = MultiHeadAttention(n_head, head_size)
+        self.ffwd = FeedFoward(n_embd)
+        self.ln1 = nn.LayerNorm(n_embd)
+        self.ln2 = nn.LayerNorm(n_embd)
+
+    
 
