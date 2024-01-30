20d25ed8b1ea760ec2ad5e45acc6bee170705f2b
Nikosane <linux.nikosane@gmail.com>
2024-01-30 23:41:23 +0530

    init weights

--- gpt.py
+++ gpt.py
@@ -139,5 +139,13 @@ class GPTLanguageModel(nn.Module):
 
         self.apply(self._init_weights)
 
+    def _init_weights(self, module):
+        if isinstance(module, nn.Linear):
+            torch.nn.init.normal_(module.weight, mean=0.0, std=0.02)
+            if module.bias is not None:
+                torch.nn.init.zeros_(module.bias)
+        elif isinstance(module, nn.Embedding):
+            torch.nn.init.normal_(module.weight, mean=0.0, std=0.02)
+
     
 
